3794819e369c65c578b67350df120bab36683ad2
jfoody6 <164776412+jfoody6@users.noreply.github.com>
2024-04-22 00:46:05 +0000

    update

--- app/controllers/roll_controller.rb
+++ app/controllers/roll_controller.rb
@@ -1,19 +1,18 @@
 class RollController < ApplicationController
 
   def roll
-
-    render({:template => "game_templates/roll"})
-
+    
     @rolls = []
 
     2.times do
       die = rand(1..6)
-  
-      @rolls.push(die)
 
-    end 
-  end
+      @rolls.push(die)
+    end
+
+    render({:template => "game_templates/roll"})
+    
+  end 
 end
 
 
- 
